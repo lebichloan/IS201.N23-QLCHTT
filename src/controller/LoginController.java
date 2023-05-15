5fcd40cbdae858b533b2c3e6e1a9a78f5f2d7acb
Vinh <21520530@gm.uit.edu.vn>
2023-05-15 09:51:05 +0700

    updated

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,16 +1,24 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
 
@@ -61,6 +69,33 @@ public class LoginController implements Initializable {
             dropShadow1.setRadius(0.0);
             loginButton.setCursor(Cursor.DEFAULT);
         });
-	}
+		
+		loginButton.setOnMouseClicked((MouseEvent event) -> {
+			try {
+			    FXMLLoader loader = new FXMLLoader(common.class.getResource("/view/TrangChu.fxml"));
+			    AnchorPane root = loader.load();
 
+			   
+
+			    // Create a Scene object with the root node and set it to the primary stage
+			    Scene scene = new Scene(root);
+
+			    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			    window.setScene(scene);
+			    window.show();
+
+			    // add a listener to the stage to handle window resize events
+			    scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> {
+			    	double scaleFactor = newSceneWidth.doubleValue() / oldSceneWidth.doubleValue();
+		            root.setPrefWidth(root.getPrefWidth() * scaleFactor);
+			    });
+			    scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> {
+			    	double scaleFactor = newSceneHeight.doubleValue() / oldSceneHeight.doubleValue();
+		            root.setPrefWidth(root.getPrefWidth() * scaleFactor);
+			    });
+			} catch (IOException e) {
+			    e.printStackTrace();
+			}
+		});
+	}
 }
--- src/controller/common.java
+++ src/controller/common.java
@@ -38,4 +38,47 @@ public class common {
             e.printStackTrace();
         }
     }
+	
+	public static void newScene (String path, ActionEvent event){
+    	try {
+    		FXMLLoader loader = new FXMLLoader(common.class.getResource(path));
+    		AnchorPane root = loader.load();
+
+            // Create a Scene object with the root node and set it to the primary stage
+            Scene scene = new Scene(root);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the Resizable property of the primary stage to true
+            window.setResizable(true);
+
+            // Set the scene to the primary stage and show it
+            window.setScene(scene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public static void newScene (String path, MouseEvent event){
+    	try {
+    		FXMLLoader loader = new FXMLLoader(common.class.getResource(path));
+    		AnchorPane root = loader.load();
+
+            // Create a Scene object with the root node and set it to the primary stage
+            Scene scene = new Scene(root);
+            Stage window = new Stage ();
+            
+            window.setHeight(768);
+            window.setWidth(1366);
+
+            // Set the Resizable property of the primary stage to true
+            window.setResizable(true);
+
+            // Set the scene to the primary stage and show it
+            window.setScene(scene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
